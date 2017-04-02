2666097d17dbeae364ff09c999f60aa44aa9eba1
qixinliang <qixinliang@maliqianbao.com>
2017-04-03 02:21:35 +0800

    我的业绩修改

--- sales/web/controller/myResultsController.php
+++ sales/web/controller/myResultsController.php
@@ -1,48 +1,27 @@
 <?php
 if (!defined('IS_INITPHP')) exit('Access Denied!');
-
-/************************************************************
- * @copyright(c): 2017年2月27日
- * @Author:  yuwen
- * @Create Time: 下午5:23:16
- * @qq:32891873
- * @email:fuyuwen88@126.com
- * @我的业绩
- *************************************************************/
+/*
+ *@我的业绩控制器
+ */
 class myResultsController extends baseController{
     public $initphp_list = array(); //Action白名单
 
-    /************************************************************
-     * @copyright(c): 2017年2月27日
-     * @Author:  yuwen
-     * @Create Time: 下午6:05:29
-     * @qq:32891873
-     * @email:fuyuwen88@126.com
-     * @构造函数自动加载
-     *************************************************************/
+	public $authService  		= NULL;
+	public $adminService 		= NULL;
+	public $myResultsService 	= NULL;
+
     public function __construct(){
         parent::__construct();
-		$this->authService = InitPHP::getService('auth');
+		$this->authService  	 = InitPHP::getService('auth');
+		$this->adminService    	 = InitPHP::getService('admin');
+		$this->myResultsService  = InitPHP::getService('myResults');
     }
-    /************************************************************
-     * @copyright(c): 2017年2月27日
-     * @Author:  yuwen
-     * @Create Time: 下午6:05:46
-     * @qq:32891873
-     * @email:fuyuwen88@126.com
-     * @我的业绩默认列表
-     *************************************************************/
     public function run(){
 		$this->authService->checkauth('1019');
-        $adminService = InitPHP::getService("admin");//获取管理员信息
-        $myResultsService = InitPHP::getService("myResults");
-        
-        $userinfo['id'] = $this->controller->get_gp('uid') ? $this->controller->get_gp('uid') : '' ;
-        if(empty($userinfo['id'])){
-            $userinfo = $adminService->current_user();
-        }
-        $res = $myResultsService->ResultsList($userinfo);
+        $userinfo = $this->adminService->current_user();
+        $res = $this->myResultsService->ResultsList($userinfo);
         $renshucount=0;
+
         if(!empty($res['data'])){
            foreach ($res['data'] as $Key=>$val){
               $renshucount+= $val['yaoqingrencount'];
@@ -50,7 +29,6 @@ class myResultsController extends baseController{
         }
         $this->view->assign('renshucount',$renshucount);
         $this->view->assign('list',$res['data']);
-        $this->view->display("myresults/run"); //使用模板
+        $this->view->display("myresults/run");
     }
-   
 }
