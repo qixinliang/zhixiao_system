424479c17214981e36deb81250adad3641847eea
qixinliang <qixinliang@github.com>
2017-04-15 18:32:21 +0800

    公告管理

--- sales/library/service/noticeService.php
+++ sales/library/service/noticeService.php
@@ -20,4 +20,25 @@ class noticeService extends Service{
 	public function add($data){
 		return $this->noticeDao->add($data);
 	}
+	
+	//根据id获取公告详情
+	public function getNoticeInfo($id){
+		return $this->noticeDao->getNoticeInfo($id);
+	}
+
+
+	//根据ID更新数据
+	public function update($data,$id){
+		return $this->noticeDao->update($data,$id);
+	}
+
+	//删除一条公告
+	public function del($id){
+		return $this->noticeDao->del($id);
+	}
+
+	//获取最新的公告
+	public function getLatestNotice(){
+		return $this->noticeDao->getLatestNotice();
+	}
 }
--- sales/web/controller/noticeController.php
+++ sales/web/controller/noticeController.php
@@ -6,7 +6,7 @@ if (!defined('IS_INITPHP')) exit('Access Denied!');
  */
 class noticeController extends baseController{
 
-	public $initphp_list = array('run','publish','publishSave');
+	public $initphp_list = array('run','publish','edit','editSave','del');
 	public $noticeService = NULL;
 
 	public function __construct(){
@@ -21,6 +21,8 @@ class noticeController extends baseController{
 		if(!isset($rows) || empty($rows)){
 			exit(json_encode(array('status' => -1,'message' => 'notice empty！')));
 		}*/
+		$latest = $this->noticeService->getLatestNotice();
+		var_dump($latest);
 
 		$this->view->assign('notice_list',$rows);
 		$this->view->display('notice/run');
@@ -28,34 +30,70 @@ class noticeController extends baseController{
 	
 	//发布公告
 	public function publish(){
-		//$title = $this->controller->get_gp('title');
-		//$content = $this->controller->get_gp('content');
-		$this->view->display('notice/publish');
-	}
-	public function publishSave(){
 		$title = $this->controller->get_gp('title');
 		$content = $this->controller->get_gp('content');
+		if(empty($title) || empty($content)){
+			$this->view->display('notice/publish');
+		}else{
+			$time = time();
+			$data = array(
+				'title' => $title,
+				'content' => $content,
+				'create_time' => $time,
+				'update_time' => $time,
+			);
+			$ret = $this->noticeService->add($data);
+			if($ret > 0){
+				exit(json_encode(array('status'=> 1,'message' => '公告发布成功！')));
+			}else{
+				exit(json_encode(array('status'=> -1,'message' => '公告发布失败！')));
+			}
 
-		$time = time();
+		}
+	}
+
+	//公告修改
+	public function edit(){
+		$id = $this->controller->get_gp('id');
+		$info = $this->noticeService->getNoticeInfo($id);
+		if(!isset($info) || empty($info)){
+			exit(json_encode(array('status' => -1,'message' => 'Notice not Found！')));
+		}	
+		$this->view->assign('info',$info);
+		$this->view->display('notice/edit');
+	}
+
+	public function editSave(){
+		$noticeId = $this->controller->get_gp('notice_id');
+		
+		$title = $this->controller->get_gp('title');
+		$content = $this->controller->get_gp('content');
 		$data = array(
 			'title' => $title,
 			'content' => $content,
-			'create_time' => $time,
-			'update_time' => $time,
+			'update_time' => time(),
 		);
-		$ret = $this->noticeService->add($data);
-		if($ret > 0){
-			exit(json_encode(array('status'=> 1,'message' => '公告发布成功！')));
-		}else{
-			exit(json_encode(array('status'=> -1,'message' => '公告发布失败！')));
-		}
-	}
-	
-	public function edit(){
 
+		$ret = $this->noticeService->update($data,$noticeId);
+		if($ret > 0){
+			exit(json_encode(array('status'=> 1,'message' => '公告修改成功！')));
+		}else{
+			exit(json_encode(array('status'=> -1,'message' => '公告修改失败！')));
+		}
 	}
 
 	//根据ID删除一个公告
 	public function del(){
+		$noticeId = $this->controller->get_gp('id');		
+
+		if(!isset($noticeId) || empty($noticeId)){
+			exit(json_encode(array('status'=> -1,'message' => 'empty notice id！')));
+		}
+		$ret = $this->noticeService->del($noticeId);
+		if($ret){
+ 			exit(json_encode(array('status' => 1, 'message' => '公告删除成功!')));
+		}else{
+		 	exit(json_encode(array('status' => -1, 'message' => '公告删除失败!')));	
+		}
 	}
 }
