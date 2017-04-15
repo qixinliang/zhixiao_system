3d4dd41721b663cacc33c87253745798b96ece59
qixinliang <qixinliang@github.com>
2017-04-15 21:35:59 +0800

    公告dao

--- sales/library/dao/noticeDao.php
+++ sales/library/dao/noticeDao.php
@@ -19,4 +19,27 @@ class noticeDao extends Dao{
 	public function add($data){
  		return $this->dao->db->insert($data, $this->tableName);
 	}
+
+	//根据id获取公告详情
+	public function getNoticeInfo($id){
+		$sql = sprintf("SELECT * FROM %s WHERE notice_id=%s",$this->tableName,$id);	
+		return $this->dao->db->get_one_sql($sql);
+	}
+
+	//更新数据
+    public function update($data,$id){
+        return $this->dao->db->update_by_field($data, array('notice_id' => $id),
+			$this->tableName);
+    }
+
+	//删除公告
+    public function del($id){
+       return $this->dao->db->delete_by_field(
+		array('notice_id' => $id), $this->tableName);
+    }
+	
+	public function getLatestNotice(){
+		$sql = sprintf("SELECT * FROM %s ORDER BY `update_time` DESC LIMIT 1",$this->tableName);
+		return $this->dao->db->get_all_sql($sql);
+	}
 }
